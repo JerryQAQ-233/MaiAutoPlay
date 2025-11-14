83752a6cba5c9fccf9309cdb11a38c8ba5197cb7
Jerry233 <117577244+JerryQAQ-233@users.noreply.github.com>
2025-11-14 18:59:50 +0800

    Gpt! oh its GPt!!!!!
    fix the issue of code readability lol

--- Cheat/AutoPlay.cs
+++ Cheat/AutoPlay.cs
@@ -68,6 +68,64 @@ namespace SinmaiAssist.Cheat
         }
 
         [HarmonyPostfix]
+        [HarmonyPatch(typeof(HoldNote), "SetAutoPlayJudge")]
+        public static void HoldNoteSetAutoPlayJudge(HoldNote __instance)
+        {
+            if (!IsAutoPlay()) return;
+            var headJudgedField = AccessTools.Field(typeof(HoldNote), "HeadJudged");
+            if ((bool)headJudgedField.GetValue(__instance)) return;
+            var appearMsecField = AccessTools.Field(typeof(NoteBase), "AppearMsec");
+            float appearMsec = (float)appearMsecField.GetValue(__instance);
+            if (NotesManager.GetCurrentMsec() > appearMsec - 4.1666665f)
+            {
+                var judgeHeadResultField = AccessTools.Field(typeof(HoldNote), "JudgeHeadResult");
+                judgeHeadResultField.SetValue(__instance, GameManager.AutoJudge());
+                AccessTools.Method(typeof(HoldNote), "PlayJudgeHeadSe").Invoke(__instance, null);
+                headJudgedField.SetValue(__instance, true);
+                AccessTools.Field(typeof(HoldNote), "BodyOn").SetValue(__instance, true);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(BreakHoldNote), "SetAutoPlayJudge")]
+        public static void BreakHoldNoteSetAutoPlayJudge(BreakHoldNote __instance)
+        {
+            if (!IsAutoPlay()) return;
+            var headJudgedField = AccessTools.Field(typeof(BreakHoldNote), "HeadJudged");
+            if ((bool)headJudgedField.GetValue(__instance)) return;
+            var appearMsecField = AccessTools.Field(typeof(NoteBase), "AppearMsec");
+            float appearMsec = (float)appearMsecField.GetValue(__instance);
+            if (NotesManager.GetCurrentMsec() > appearMsec - 4.1666665f)
+            {
+                var judgeHeadResultField = AccessTools.Field(typeof(BreakHoldNote), "JudgeHeadResult");
+                judgeHeadResultField.SetValue(__instance, GameManager.AutoJudge());
+                AccessTools.Method(typeof(BreakHoldNote), "PlayJudgeHeadSe").Invoke(__instance, null);
+                headJudgedField.SetValue(__instance, true);
+                AccessTools.Field(typeof(BreakHoldNote), "BodyOn").SetValue(__instance, true);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(TouchHoldC), "SetAutoPlayJudge")]
+        public static void TouchHoldCSetAutoPlayJudge(TouchHoldC __instance)
+        {
+            if (!IsAutoPlay()) return;
+            var headJudgedField = AccessTools.Field(typeof(TouchHoldC), "HeadJudged");
+            if ((bool)headJudgedField.GetValue(__instance)) return;
+            var appearMsecField = AccessTools.Field(typeof(NoteBase), "AppearMsec");
+            float appearMsec = (float)appearMsecField.GetValue(__instance);
+            if (NotesManager.GetCurrentMsec() > appearMsec - 4.1666665f)
+            {
+                var judgeHeadResultField = AccessTools.Field(typeof(TouchHoldC), "JudgeHeadResult");
+                judgeHeadResultField.SetValue(__instance, GameManager.AutoJudge());
+                AccessTools.Method(typeof(TouchHoldC), "PlayJudgeHeadSe").Invoke(__instance, null);
+                headJudgedField.SetValue(__instance, true);
+                AccessTools.Field(typeof(TouchHoldC), "BodyOn").SetValue(__instance, true);
+                AccessTools.Field(typeof(TouchHoldC), "TriggerOn").SetValue(__instance, true);
+            }
+        }
+
+        [HarmonyPostfix]
         [HarmonyPatch(typeof(GameManager), "AutoJudge")]
         public static void AutoJudge(ref NoteJudge.ETiming __result)
         {
@@ -153,14 +211,17 @@ namespace SinmaiAssist.Cheat
         [HarmonyPatch(typeof(NoteBase), "SetAutoPlayJudge")]
         public static bool NoteBaseAutoPlayJudge(NoteBase __instance)
         {
-            if (!IsAutoPlay()) return true;
+            if (!GameManager.IsAutoPlay()) return true;
+            if (__instance is HoldNote) return true;
+            if (__instance is BreakHoldNote) return true;
+            if (__instance is TouchHoldC) return true;
             var appearMsec = (float)AccessTools.Field(typeof(NoteBase), "AppearMsec").GetValue(__instance);
             var isExNote = (bool)AccessTools.Field(typeof(NoteBase), "IsExNote").GetValue(__instance);
             var judgeType = (NoteJudge.EJudgeType)AccessTools.Field(typeof(NoteBase), "JudgeType").GetValue(__instance);
             var isBreakNote = judgeType == NoteJudge.EJudgeType.Break;
             var playJudgeSeMethod = AccessTools.Method(typeof(NoteBase), "PlayJudgeSe");
 
-            if (NotesManager.GetCurrentMsec() > appearMsec - 4.1666665f && IsAutoPlay())
+            if (NotesManager.GetCurrentMsec() > appearMsec - 4.1666665f && GameManager.IsAutoPlay())
             {
                 if ((autoPlayMode == AutoPlayMode.RandomAllPerfect || autoPlayMode == AutoPlayMode.RandomFullCombo || autoPlayMode == AutoPlayMode.RandomFullComboPlus) && (isExNote || isBreakNote))
                 {
@@ -179,7 +240,7 @@ namespace SinmaiAssist.Cheat
         [HarmonyPatch(typeof(SlideRoot), "Judge")]
         public static bool SlideRootJudge(SlideRoot __instance, ref bool __result)
         {
-            if (!IsAutoPlay()) return true;
+            if (!GameManager.IsAutoPlay()) return true;
             var isNoteCheckTimeStartMethod = AccessTools.Method(typeof(SlideRoot), "IsNoteCheckTimeStart");
             bool isNoteCheckTimeStart = (bool)isNoteCheckTimeStartMethod.Invoke(__instance, new object[] { Singleton<GamePlayManager>.Instance.GetGameScore(__instance.MonitorId).UserOption.GetJudgeTimingFrame() });
             if (!isNoteCheckTimeStart)
